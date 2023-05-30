dc8d84558dc9283e16e0979516b1c333e354cc41
Aarzoo Jhamb <aarzoo22008@iiitd.ac.in>
2023-05-30 23:05:32 +0530

    overflow

--- SimpleSimulator/stimulator.py
+++ SimpleSimulator/stimulator.py
@@ -1,12 +1,6 @@
 
 import sys
 from parameters2 import opcode2,registers2
-# import os
-
-
-# current_dir = os.getcwd()
-# file_path = os.path.join(current_dir, 'stdin.txt')
-# print(file_path)
 
 f=open("stdin.txt","r")
 
@@ -28,35 +22,81 @@ def binary_actual_op(b):  # binary to actual opcode
         if x==b:
             return y
         
-def binary_actual_r(a):   #binary to actual register
+def binary_actual_r(a):   # binary to actual register
     for x,y in registers2.items():
         if x==a:
             return y
       
-def binary_decimal(n):    #binary to decimal
-    return bin(n).replace("0b","")
+def decimal_binary(n):  
+    x=bin(n).replace("0b","")
+    return x.zfill(16)
 
-def decimal_binary(n):    #decimal to binary
+def binary_decimal(n):    
     x=int(n,2)
-    return x.zfill(8)
+    return x
 
+def decimal_binary_pc(k):
+    y=bin(k).replace("0b","")  
+    return y.zfill(7)
 
 #Type A instructions
-# def add_inst():
+def add_op(inst,pc):
+    out=""
+    count=decimal_binary_pc(pc)
+    destination=binary_actual_r(inst[7:10])
+    print(destination)
+    source_1=binary_actual_r(inst[10:13])
+    print(source_1)
+    source_2=binary_actual_r(inst[13:16])
+    print(source_2)
+    d_source_1=binary_decimal(inst[10:13])
+    d_source_2=binary_decimal(inst[13:16])
+    r_values[source_1]=decimal_binary(d_source_1)
+    r_values[source_2]=decimal_binary(d_source_2)
+    sum=d_source_1+d_source_2
+    if sum>((2**16)-1):
+        print("Overflow")
+        #overflow condition
+    else:
+        r_values[destination]=decimal_binary(sum)
+        out=count
+        for y in r_values.values():
+            out+=" "
+            out+=y  
+        return out
 
-# def sub_inst():
 
-# def mul_inst():
+# def sub_op():
 
-# def xor_inst():
+# def mul_op():
 
-# def or_inst():
+# def xor_op():
 
-# def and_inst():
+# def or_op():
+
+# def and_op():
+
+
+
+# Main function
 for line in f:
-    inst=line
-    print(inst)
-    print("\n")
+    inst=line.rstrip("\n")
+    result=binary_actual_op(inst[0:5])
+    if result=="add":
+        final_result=add_op(inst,pc)
+        pc+=1
+        print(final_result)
+
+    
+
+
+
+
+
+
+
+
+
 
 f.close()
 
