dceb1300504f25ab9b2bf1779581ecd28986973d
Aarzoo Jhamb <aarzoo22008@iiitd.ac.in>
2023-05-30 22:06:53 +0530

    start

--- SimpleSimulator/stimulator.py
+++ SimpleSimulator/stimulator.py
@@ -1,6 +1,7 @@
 
 import sys
 from parameters2 import opcode2,registers2,type2
+f=open("stdin.txt","r")
 
 r_values={
     "R0":"0"*16,
@@ -14,3 +15,45 @@ r_values={
 }
 pc=0
 halted=False
+
+def binary_actual_op(b):  # binary to actual opcode
+    for x,y in opcode2.items():
+        if x==b:
+            return y
+        
+def binary_actual_r(a):   #binary to actual register
+    for x,y in registers2.items():
+        if x==a:
+            return y
+      
+def binary_decimal(n):    #binary to decimal
+    return bin(n).replace("0b","")
+
+def decimal_binary(n):    #decimal to binary
+    x=int(n,2)
+    return x.zfill(8)
+
+
+#Type A instructions
+# def add_inst():
+
+# def sub_inst():
+
+# def mul_inst():
+
+# def xor_inst():
+
+# def or_inst():
+
+# def and_inst():
+for line in f:
+    inst=line
+    print(inst)
+    print("\n")
+
+f.close()
+
+
+
+
+
