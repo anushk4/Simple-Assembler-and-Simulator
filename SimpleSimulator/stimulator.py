92fcf5fb2bb79cc2d5a8e01a3ae738d08175ae62
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-05-31 23:48:44 +0530

    Function modification

--- SimpleSimulator/stimulator.py
+++ SimpleSimulator/stimulator.py
@@ -2,29 +2,17 @@
 import sys
 from parameters2 import opcode2,registers2
 
-f=open("stdin.txt","r")
+# def binary_actual_op(b):  # binary to actual opcode
+#     for x,y in opcode2.items():
+#         if x==b:
+#             return y
 
-r_values={
-    "R0":"0"*16,
-    "R1":"0"*16,
-    "R2":"0"*16,
-    "R3":"0"*16,
-    "R4":"0"*16,
-    "R5":"0"*16,
-    "R6":"0"*16,
-    "FLAGS":"0"*16
-}
+def get_registers_A(inst):
+    return registers2[inst[7:10]],registers2[inst[10:13]],registers2[inst[13:16]]
 
-memory = {}
+def get_registers_B(inst):
+    return registers2[inst[6:9]],inst[9:16].zfill(16)
 
-pc=0
-halted=False
-
-def binary_actual_op(b):  # binary to actual opcode
-    for x,y in opcode2.items():
-        if x==b:
-            return y
-        
 def binary_actual_r(a):   # binary to actual register
     for x,y in registers2.items():
         if x==a:
@@ -46,88 +34,60 @@ def decimal_binary_pc(k):
 def add_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    print(destination)
-    source_1=binary_actual_r(inst[10:13])
-    print(source_1)
-    source_2=binary_actual_r(inst[13:16])
-    print(source_2)
-    d_source_1=binary_decimal(inst[10:13])
-    d_source_2=binary_decimal(inst[13:16])
-    r_values[source_1]=decimal_binary(d_source_1)
-    r_values[source_2]=decimal_binary(d_source_2)
+    destination,source_1,source_2=get_registers_A(inst)
+    d_source_1=binary_decimal(r_values[source_1])
+    d_source_2=binary_decimal(r_values[source_2])
     final=d_source_1+d_source_2
-    if final>((2**16)-1):
-        print("Overflow")
-        #overflow condition
-    else:
-        r_values[destination]=decimal_binary(final)
-        out=count
-        for y in r_values.values():
-            out+=" "
-            out+=y  
-        return out
+    if not (final >= 0 and final<=((2**16)-1)):
+        final=0
+        r_values["FLAGS"]=flag_overflow
+    r_values[destination]=decimal_binary(final)
+    out=count
+    for y in r_values.values():
+        out+=" "
+        out+=y  
+    return out
 
 def sub_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    print(destination)
-    source_1=binary_actual_r(inst[10:13])
-    print(source_1)
-    source_2=binary_actual_r(inst[13:16])
-    print(source_2)
-    d_source_1=binary_decimal(inst[10:13])
-    d_source_2=binary_decimal(inst[13:16])
-    r_values[source_1]=decimal_binary(d_source_1)
-    r_values[source_2]=decimal_binary(d_source_2)
+    destination,source_1,source_2=get_registers_A(inst)
+    d_source_1=binary_decimal(r_values[source_1])
+    d_source_2=binary_decimal(r_values[source_2])
     final=d_source_1-d_source_2
-    if final>((2**16)-1):
-        print("Overflow")
-        #overflow condition
-    else:
-        r_values[destination]=decimal_binary(final)
-        out=count
-        for y in r_values.values():
-            out+=" "
-            out+=y  
-        return out
+    if not (final >= 0 and final<=((2**16)-1)):
+        final=0
+        r_values["FLAGS"]=flag_overflow
+    r_values[destination]=decimal_binary(final)
+    out=count
+    for y in r_values.values():
+        out+=" "
+        out+=y  
+    return out
 
 def mul_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    print(destination)
-    source_1=binary_actual_r(inst[10:13])
-    print(source_1)
-    source_2=binary_actual_r(inst[13:16])
-    print(source_2)
-    d_source_1=binary_decimal(inst[10:13])
-    d_source_2=binary_decimal(inst[13:16])
-    r_values[source_1]=decimal_binary(d_source_1)
-    r_values[source_2]=decimal_binary(d_source_2)
+    destination,source_1,source_2=get_registers_A(inst)
+    d_source_1=binary_decimal(r_values[source_1])
+    d_source_2=binary_decimal(r_values[source_2])
     final=d_source_1*d_source_2
-    if final>((2**16)-1):
-        print("Overflow")
-        #overflow condition
-    else:
-        r_values[destination]=decimal_binary(final)
-        out=count
-        for y in r_values.values():
-            out+=" "
-            out+=y  
-        return out
+    if not (final >= 0 and final<=((2**16)-1)):
+        final=0
+        r_values["FLAGS"]=flag_overflow
+    r_values[destination]=decimal_binary(final)
+    out=count
+    for y in r_values.values():
+        out+=" "
+        out+=y  
+    return out
 
 def xor_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    source_1=binary_actual_r(inst[10:13])
-    source_2=binary_actual_r(inst[13:16])
-    d_source_1=inst[10:13].zfill(16)
-    d_source_2=inst[13:16].zfill(16)
-    r_values[source_1]=d_source_1
-    r_values[source_2]=d_source_2
+    destination,source_1,source_2=get_registers_A(inst)
+    d_source_1=r_values[source_1]
+    d_source_2=r_values[source_2]
     final=d_source_1^d_source_2
     r_values[destination]=final
     out=count
@@ -139,13 +99,9 @@ def xor_op(inst,pc):
 def or_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    source_1=binary_actual_r(inst[10:13])
-    source_2=binary_actual_r(inst[13:16])
-    d_source_1=inst[10:13].zfill(16)
-    d_source_2=inst[13:16].zfill(16)
-    r_values[source_1]=d_source_1
-    r_values[source_2]=d_source_2
+    destination,source_1,source_2=get_registers_A(inst)
+    d_source_1=r_values[source_1]
+    d_source_2=r_values[source_2]
     final=d_source_1|d_source_2
     r_values[destination]=final
     out=count
@@ -157,9 +113,7 @@ def or_op(inst,pc):
 def and_op(inst,pc):
     out=""
     count=decimal_binary_pc(pc)
-    destination=binary_actual_r(inst[7:10])
-    source_1=binary_actual_r(inst[10:13])
-    source_2=binary_actual_r(inst[13:16])
+    destination,source_1,source_2=get_registers_A(inst)
     d_source_1=inst[10:13].zfill(16)
     d_source_2=inst[13:16].zfill(16)
     r_values[source_1]=d_source_1
@@ -177,8 +131,7 @@ def and_op(inst,pc):
 def mov_imm_op(inst, pc):
     out = ""
     count = decimal_binary_pc(pc)
-    destination = binary_actual_r(inst[6:9])
-    immediate_value = inst[9:16].zfill(16)
+    destination,immediate_value=get_registers_B(inst)
     r_values[destination] = immediate_value
     out = count
     for y in r_values.values():
@@ -203,40 +156,107 @@ def mov_reg_op(inst, pc):
 
 # Type D instruction
 
-def ld_op(inst, pc):
-    out = ""
-    count = decimal_binary_pc(pc)
-    destination = binary_actual_r(inst[7:10])
-    memory_address = binary_actual_r(inst[10:13])
-    r_values[destination] = memory[memory_address]
-    out = count
-    for y in r_values.values():
-        out += " "
-        out += y
-    return out
+# def ld_op(inst, pc):
+#     out = ""
+#     count = decimal_binary_pc(pc)
+#     destination = binary_actual_r(inst[7:10])
+#     memory_address = binary_actual_r(inst[10:13])
+#     r_values[destination] = memory[memory_address]
+#     out = count
+#     for y in r_values.values():
+#         out += " "
+#         out += y
+#     return out
 
-def st_op(inst, pc):
-    out = ""
-    count = decimal_binary_pc(pc)
-    source = binary_actual_r(inst[7:10])
-    memory_address = binary_actual_r(inst[10:13])
-    memory[memory_address] = r_values[source]
-    out = count
-    for y in r_values.values():
-        out += " "
-        out += y
-    return out
+# def st_op(inst, pc):
+#     out = ""
+#     count = decimal_binary_pc(pc)
+#     source = binary_actual_r(inst[7:10])
+#     memory_address = binary_actual_r(inst[10:13])
+#     memory[memory_address] = r_values[source]
+#     out = count
+#     for y in r_values.values():
+#         out += " "
+#         out += y
+#     return out
 
 # Main function
 
-for line in f:
-    inst=line.rstrip("\n")
-    result=binary_actual_op(inst[0:5])
-    if result=="add":
-        final_result=add_op(inst,pc)
-        pc+=1
-        print(final_result)
+#input for testing
+ 
+f=open("stdin.txt","r")
 
+# lines = []
+
+# actual input
+
+# while True:
+#     line = sys.stdin.readline().strip()
+#     if not line:
+#         break
+#     lines.append(line)
+
+r_values={
+    "R0":"0"*16,
+    "R1":"0"*16,
+    "R2":"0"*16,
+    "R3":"0"*16,
+    "R4":"0"*16,
+    "R5":"0"*16,
+    "R6":"0"*16,
+    "FLAGS":"0"*16
+}
+
+k=0
+file_instructions={}
+pointers=[]
+for i in f:
+    pointer=bin(k)[2:].zfill(7)
+    pointers.append(pointer)
+    file_instructions[pointer]=i.rstrip("\n")
+    k+=1
+
+pc=0
+halted=False
+
+#Flag initialisation
+flag_overflow="0"*12+"1000"
+flag_less_than="0"*12+"0100"
+flag_greater_than="0"*12+"0010"
+flag_equal="0"*12+"0001"
+
+i=0
+inst=file_instructions[pointers[i]]
+while not halted:
+    op_code=inst[0:5]
+    if op_code=="0"*5:
+        final_result=add_op(inst,pc)
+        print(final_result)
+    elif op_code=="00001":
+        final_result=sub_op(inst,pc)
+        print(final_result)
+    elif op_code=="00110":
+        final_result=mul_op(inst,pc)
+        print(final_result)
+    elif op_code=="01010":
+        final_result=xor_op(inst,pc)
+        print(final_result)
+    elif op_code=="01011":
+        final_result=or_op(inst,pc)
+        print(final_result)
+    elif op_code=="01100":
+        final_result=and_op(inst,pc)
+        print(final_result)
+    elif op_code=="00010":
+        final_result=mov_imm_op(inst,pc)
+        print(final_result)
+    elif op_code=="11010":
+        halted=True
+        break
+    if op_code not in ["11100","11101","01111","11111"]:
+        i+=1
+        inst=file_instructions[pointers[i]]
+    pc+=1
     
 
 
